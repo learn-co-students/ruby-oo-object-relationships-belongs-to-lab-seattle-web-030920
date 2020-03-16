5b65870744d418e38c2415d379b2c141f7689444
djknudson <djknudson@icloud.com>
2020-03-16 11:33:13 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+# require_relative 'artist.rb'
+class Song
+    attr_accessor :artist, :title
+
+    def initialize#(aritist, title)
+        # @artist = artist
+        # @title = title
+    end
+end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+# require_relative 'song.rb'
 
 describe "Song" do
   let!(:song) { Song.new } 
